875da4a3415a98b26aac6ff1608a7abecf9c76f5
Vlas Sokolov <vlas145@gmail.com>
2016-04-12 15:12:56 +0200

    Signal/noise cubes can be written as FITS files.

--- astro_toolbox.py
+++ astro_toolbox.py
@@ -5,7 +5,8 @@ import os
 # TODO: rewrite this to have multiple components generated here.
 #       Having a bundle of test filaments would be very nice.
 
-def make_test_cube(shape=(30,9,9), outfile='test.fits', sigma=None, seed=0):
+def make_test_cube(shape=(30,9,9), outfile='test.fits', 
+                   sigma=None, seed=0, writeSN=False   ):
     """
     Generates a simple gaussian cube with noise of
     given shape and writes it as a fits file.
@@ -19,13 +20,13 @@ def make_test_cube(shape=(30,9,9), outfile='test.fits', sigma=None, seed=0):
     gauss1d = Gaussian1DKernel(stddev=sigma1d, x_size=shape[0])
     gauss2d = Gaussian2DKernel(stddev=sigma2d, x_size=shape[1], 
                                                y_size=shape[2])
-    test_cube = gauss1d.array[:,None,None] * gauss2d.array
-    test_cube=test_cube/test_cube.max()
+    signal_cube = gauss1d.array[:,None,None] * gauss2d.array
+    signal_cube=signal_cube/signal_cube.max()
     # adding noise:
     np.random.seed(seed)
-    noise_cube = (np.random.random(test_cube.shape)-.5)* \
-                        np.median(test_cube.std(axis=0))
-    test_cube += noise_cube
+    noise_cube = (np.random.random(signal_cube.shape)-.5)* \
+                        np.median(signal_cube.std(axis=0))
+    test_cube = signal_cube+noise_cube
     true_rms = noise_cube.std()
     
     # making a simple header for the test cube:
@@ -49,6 +50,14 @@ def make_test_cube(shape=(30,9,9), outfile='test.fits', sigma=None, seed=0):
     test_header.update(keylist)
     test_hdu = fits.PrimaryHDU(data=test_cube, header=test_header)
     test_hdu.writeto(outfile, clobber=True, checksum=True)
+
+    if writeSN:
+        signal_hdu = fits.PrimaryHDU(data=signal_cube, header=test_header)
+        noise_hdu  = fits.PrimaryHDU(data=noise_cube , header=test_header)
+        signame, noiname = [outfile.split('.fits')[0]+'-'+i+'.fits' 
+                                            for i in ['signal','noise']]
+        signal_hdu.writeto(signame, clobber=True, checksum=True)
+        noise_hdu.writeto( noiname, clobber=True, checksum=True)
 
 def download_test_cube(outfile='test.fits'):
     """
