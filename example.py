a90680d1ead98e6b91ea56213af0e8dfdd2cc1e2
Vlas Sokolov <vlas145@gmail.com>
2016-04-14 22:36:23 +0200

    a comparison between single guess and a guess map

--- example.py
+++ example.py
@@ -9,6 +9,7 @@ import matplotlib.pylab as plt
 import pyspeckit
 from subcube import SubCube
 from astro_toolbox import make_test_cube
+from astro_toolbox import get_ncores
 
 # TODO: rewrite into a jupyter notebook example!
 
@@ -47,25 +48,47 @@ print "Generating spectral models for all %i guesses . . ." \
 sc.generate_model()
 print "Calculating the best guess on the grid . . ."
 sc.best_guess()
-
-from astro_toolbox import get_ncores
 # TODO: why does 'fixed' fitkwarg break fiteach?
 sc.fiteach_args.pop('fixed',None)
-sc.fiteach(fittype               = sc.fittype,
-           guesses               = sc.guess_grid[sc.best_map].T,
-           multicore             = get_ncores(),
-           errmap                = sc._rms_map,
-           **sc.fiteach_args)
+
+sc1, sc2 = sc, sc.copy()
+
+# fitting the whole cube with a single guess, selected
+# to be the best for the pixel with the highest S/N ratio
+sc1.fiteach(fittype   = sc1.fittype,
+            guesses   = sc1.guess_grid[sc1.best_model],
+            multicore = get_ncores(),
+            errmap    = sc1._rms_map,
+            **sc1.fiteach_args)
 
 # computing chi^2 statistics to judge the goodness of fit:
-sc.get_chi_squared(sigma=sc.header['RMSLVL'])
-sc.chi_squared_stats()
+sc1.get_chi_squared(sigma=sc1.header['RMSLVL'])
+sc1.chi_squared_stats()
 
 # let's plot the velocity field:
-sc.show_fit_param(1, cmap='coolwarm')
-clb = sc.mapplot.FITSFigure.colorbar
-clb.set_axis_label_text(sc.xarr.unit.to_string('latex_inline'))
+sc1.show_fit_param(1, cmap='coolwarm')
+clb = sc1.mapplot.FITSFigure.colorbar
+clb.set_axis_label_text(sc1.xarr.unit.to_string('latex_inline'))
 
 # and overlay the pixels that didn't converge properly:
-sc.mark_bad_fits()
+sc1.mark_bad_fits() # not so good at the edges, right? 
+# (the velocity centroid varies too much across the map)
+
+# now let's pass fiteach the best guesses for every x,y pixel!
+sc2.fiteach(fittype   = sc2.fittype,
+            guesses   = sc2.guess_grid[sc2.best_map].T,
+            multicore = get_ncores(),
+            errmap    = sc2._rms_map,
+            **sc2.fiteach_args)
+
+# computing chi^2 statistics to judge the goodness of fit:
+sc2.get_chi_squared(sigma=sc2.header['RMSLVL'], refresh=True)
+sc2.chi_squared_stats()
+
+sc2.show_fit_param(1, cmap='coolwarm')
+clb = sc2.mapplot.FITSFigure.colorbar
+clb.set_axis_label_text(sc2.xarr.unit.to_string('latex_inline'))
+
+# and overlay the pixels that didn't converge properly:
+sc2.mark_bad_fits() # voila! all the pixels are fit!
 plt.show()
