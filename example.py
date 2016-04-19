66253a7dcd488246929e74c74c47dddbef3ccb75
Vlas Sokolov <vlas145@gmail.com>
2016-04-19 16:06:47 +0200

    secondary component + spectrum displayed!

--- example.py
+++ example.py
@@ -21,13 +21,16 @@ sc = SubCube('foo.fits')
 #       as a general synthetic cube generator routine
 # let's tinker with the cube a bit!
 # this will introduce a radial velocity gradient:
-def rotate_ppv(arr):
+def tinker_ppv(arr):
     scale_roll = 15
+    rel_shift  = 30
+    rel_str    = 5
+    shifted_component = np.roll(arr, rel_shift) / rel_str
     for y,x in np.ndindex(arr.shape[1:]):
-        roll = np.sqrt((x-5)**2 + (y-5)**2) * scale_roll
+        roll  = np.sqrt((x-5)**2 + (y-5)**2) * scale_roll
         arr[:,y,x] = np.roll(arr[:,y,x], int(roll))
-    return arr
-sc.cube = rotate_ppv(sc.cube)
+    return arr + shifted_component
+sc.cube = tinker_ppv(sc.cube)
 
 sc.update_model('gaussian')
 
@@ -39,10 +42,7 @@ print "Estimating SNR . . ."
 sc.get_snr_map()
 
 print "Making a guess grid based on parameter permutations . . ."
-sc.make_guess_grid(minpars, maxpars, finesse,
-#                   limitedmin = [True, False, True],
-#                   limitedmax = [True, False, True],
-                )
+sc.make_guess_grid(minpars, maxpars, finesse)
 print "Generating spectral models for all %i guesses . . ." \
                     % sc.guess_grid.shape[0]
 sc.generate_model()
@@ -61,15 +61,15 @@ sc1.fiteach(fittype   = sc1.fittype,
 
 # computing chi^2 statistics to judge the goodness of fit:
 sc1.get_chi_squared(sigma=sc1.header['RMSLVL'])
-sc1.chi_squared_stats()
+sc1.chi_squared_stats(plot_chisq=True)
 
 # let's plot the velocity field:
-sc1.show_fit_param(1, cmap='coolwarm')
+#sc1.show_fit_param(1, cmap='coolwarm')
 clb = sc1.mapplot.FITSFigure.colorbar
 clb.set_axis_label_text(sc1.xarr.unit.to_string('latex_inline'))
 
 # and overlay the pixels that didn't converge properly:
-sc1.mark_bad_fits() # not so good at the edges, right? 
+sc1.mark_bad_fits(cut = 1e-40) # not so good at the edges, right?
 # (the velocity centroid varies too much across the map)
 
 # now let's pass fiteach the best guesses for every x,y pixel!
@@ -88,5 +88,5 @@ clb = sc2.mapplot.FITSFigure.colorbar
 clb.set_axis_label_text(sc2.xarr.unit.to_string('latex_inline'))
 
 # and overlay the pixels that didn't converge properly:
-sc2.mark_bad_fits() # voila! all the pixels are fit!
+sc2.mark_bad_fits(cut = 1e-40) # voila! all the pixels are fit!
 plt.show()
