74102771822c2273c3fae34d110250beb5f82c80
Vlas Sokolov <vlas145@gmail.com>
2016-04-30 12:28:51 +0200

    copied fiteach_mod from pyspeckit

--- subcube.py
+++ subcube.py
@@ -6,6 +6,10 @@ from astropy.utils.console import ProgressBar
 import pyspeckit
 import os
 
+# imports for the test fiteach redefinition
+import time, itertools
+from astropy.extern.six import string_types
+
 class SubCube(pyspeckit.Cube):
     """
     An extension of Cube, tinkered to be an instance of MultiCube, from which 
@@ -667,6 +671,302 @@ class SubCube(pyspeckit.Cube):
 
     #    return np.log(likelihood)
 
+    # taken directly from pyspeckit.cubes.fiteach()!
+    # reduced the function a bit, will tiner 
+    # dynamically selected guesses soon.
+    def fiteach_mod(self, errmap=None, guesses=(), verbose=True,
+                    verbose_level=1, quiet=True, signal_cut=3,
+                    usemomentcube=None, blank_value=0,
+                    use_neighbor_as_guess=False, start_from_point=(0,0),
+                    multicore=1, position_order=None, maskmap=None, 
+                    **fitkwargs):
+        """
+        Fit a spectrum to each valid pixel in the cube
+
+        For guesses, priority is *use_nearest_as_guess*, *usemomentcube*,
+        *guesses*, None
+
+        Once you have successfully run this function, the results will be
+        stored in the ``.parcube`` and ``.errcube`` attributes, which are each
+        cubes of shape ``[npars, ny, nx]``, where npars is the number of fitted
+        parameters and ``nx``, ``ny`` are the shape of the map.  ``errcube``
+        contains the errors on the fitted parameters (1-sigma, as returned from
+        the Levenberg-Marquardt fit's covariance matrix).  You can use the
+        attribute ``has_fit``, which is a map of shape ``[ny,nx]`` to find
+        which pixels have been successfully fit.
+
+
+        Parameters
+        ----------
+        use_neighbor_as_guess: bool
+            Set this keyword to use the average best-fit parameters from
+            neighboring positions with successful fits as the guess
+        start_from_point: tuple(int,int)
+            Either start from the center or from a point defined by a tuple.
+            Work outward from that starting point.
+        position_order: ndarray[naxis=2]
+            2D map of region with pixel values indicating the order in which
+            to carry out the fitting.  Any type with increasing pixel values.
+        guesses: tuple or ndarray[naxis=3]
+            Either a tuple/list of guesses with len(guesses) = npars or a cube
+            of guesses with shape [npars, ny, nx].
+        signal_cut: float
+            Minimum signal-to-noise ratio to "cut" on (i.e., if peak in a given
+            spectrum has s/n less than this value, ignore it)
+        blank_value: float
+            Value to replace non-fitted locations with.  A good alternative is
+            numpy.nan
+        verbose: bool
+        verbose_level: int
+            Controls how much is output.
+            0,1 - only changes frequency of updates in loop
+            2 - print out messages when skipping pixels
+            3 - print out messages when fitting pixels
+            4 - specfit will be verbose
+        multicore: int
+            if >1, try to use multiprocessing via parallel_map to run on
+            multiple cores
+        maskmap : `np.ndarray`, optional
+            A boolean mask map, where ``True`` implies that the data are good.
+            This will be used for both plotting using `mapplot` and fitting
+            using `fiteach`.  If ``None``, will use ``self.maskmap``.
+        """
+        if not hasattr(self.mapplot,'plane'):
+            self.mapplot.makeplane()
+
+        if maskmap is None:
+            maskmap = self.maskmap
+
+        yy,xx = np.indices(self.mapplot.plane.shape)
+        if isinstance(self.mapplot.plane, np.ma.core.MaskedArray):
+            OK = ((~self.mapplot.plane.mask) &
+                  maskmap.astype('bool')).astype('bool')
+        else:
+            OK = (np.isfinite(self.mapplot.plane) &
+                  maskmap.astype('bool')).astype('bool')
+
+        # NAN guesses rule out the model too
+        if hasattr(guesses,'shape') and guesses.shape[1:] == self.cube.shape[1:]:
+            bad = np.isnan(guesses).sum(axis=0).astype('bool')
+            OK &= (~bad)
+
+        if start_from_point == 'center':
+            start_from_point = (xx.max()/2., yy.max/2.)
+        if hasattr(position_order,'shape') and position_order.shape == self.cube.shape[1:]:
+            sort_distance = np.argsort(position_order.flat)
+        else:
+            d_from_start = ((xx-start_from_point[1])**2 + (yy-start_from_point[0])**2)**0.5
+            sort_distance = np.argsort(d_from_start.flat)
+
+        valid_pixels = list(zip(xx.flat[sort_distance][OK.flat[sort_distance]],
+                                yy.flat[sort_distance][OK.flat[sort_distance]]))
+
+        if len(valid_pixels) != len(set(valid_pixels)):
+            raise ValueError("There are non-unique pixels in the 'valid pixel' list.  "
+                             "This should not be possible and indicates a major error.")
+        elif len(valid_pixels) == 0:
+            raise ValueError("No valid pixels selected.")
+
+        if verbose_level > 0:
+            log.debug("Number of valid pixels: %i" % len(valid_pixels))
+
+        guesses_are_moments = (isinstance(guesses, string_types) and
+                                 guesses in ('moment','moments'))
+        if guesses_are_moments or (usemomentcube and len(guesses)):
+            if not hasattr(self, 'momentcube') and guesses_are_moments:
+                self.momenteach()
+            npars = self.momentcube.shape[0]
+        else:
+            npars = len(guesses)
+            if npars == 0:
+                raise ValueError("Parameter guesses are required.")
+
+        self.parcube = np.zeros((npars,)+self.mapplot.plane.shape)
+        self.errcube = np.zeros((npars,)+self.mapplot.plane.shape)
+
+        # newly needed as of March 27, 2012.  Don't know why.
+        if 'fittype' in fitkwargs:
+            self.specfit.fittype = fitkwargs['fittype']
+        self.specfit.fitter = self.specfit.Registry.multifitters[self.specfit.fittype]
+
+        # TODO: VALIDATE THAT ALL GUESSES ARE WITHIN RANGE GIVEN THE
+        # FITKWARG LIMITS
+
+        # array to store whether pixels have fits
+        self.has_fit = np.zeros(self.mapplot.plane.shape, dtype='bool')
+
+        self._counter = 0
+
+        t0 = time.time()
+
+        def fit_a_pixel(iixy):
+            ii,x,y = iixy
+            sp = self.get_spectrum(x,y)
+
+            # very annoying - cannot use min/max without checking type
+            # maybe can use np.asarray here?
+            # cannot use sp.data.mask because it can be a scalar boolean,
+            # which does unpredictable things.
+            if hasattr(sp.data, 'mask') and not isinstance(sp.data.mask, (bool,
+                                                                          np.bool_)):
+                sp.data[sp.data.mask] = np.nan
+                sp.error[sp.data.mask] = np.nan
+                sp.data = np.array(sp.data)
+                sp.error = np.array(sp.error)
+
+            elif errmap is not None:
+                sp.error = np.ones(sp.data.shape) * errmap[y,x]
+            else:
+                if verbose_level > 1 and ii==0:
+                    log.warn("using data std() as error.")
+                sp.error[:] = sp.data[sp.data==sp.data].std()
+            if sp.error is not None and signal_cut > 0:
+                snr = sp.data / sp.error
+                max_sn = np.nanmax(snr)
+                if max_sn < signal_cut:
+                    if verbose_level > 1:
+                        log.info("Skipped %4i,%4i (s/n=%0.2g)" % (x,y,max_sn))
+                    return
+                elif np.isnan(max_sn):
+                    if verbose_level > 1:
+                        log.info("Skipped %4i,%4i (s/n is nan; max(data)=%0.2g, min(error)=%0.2g)" %
+                                 (x,y,np.nanmax(sp.data),np.nanmin(sp.error)))
+                    return
+                if verbose_level > 2:
+                    log.info("Fitting %4i,%4i (s/n=%0.2g)" % (x,y,max_sn))
+            else:
+                max_sn = None
+            sp.specfit.Registry = self.Registry # copy over fitter registry
+
+            # Do some homework for local fits
+            # Exclude out of bounds points
+            xpatch, ypatch = get_neighbors(x,y,self.has_fit.shape)
+            local_fits = self.has_fit[ypatch+y,xpatch+x]
+
+            if use_neighbor_as_guess and np.any(local_fits):
+                # Array is N_guess X Nvalid_nbrs so averaging over
+                # Axis=1 is the axis of all valid neighbors
+                gg = np.mean(self.parcube[:, (ypatch+y)[local_fits],
+                                          (xpatch+x)[local_fits]], axis=1)
+            elif hasattr(guesses,'shape') and guesses.shape[1:] == self.cube.shape[1:]:
+                if verbose_level > 1 and ii == 0:
+                    log.info("Using input guess cube")
+                gg = guesses[:,y,x]
+            else:
+                if verbose_level > 1 and ii == 0:
+                    log.info("Using input guess")
+                gg = guesses
+
+            if np.all(np.isfinite(gg)):
+                try:
+                    sp.specfit(guesses=gg, quiet=verbose_level<=3,
+                               verbose=verbose_level>3, **fitkwargs)
+                    self.parcube[:,y,x] = sp.specfit.modelpars
+                    self.errcube[:,y,x] = sp.specfit.modelerrs
+                    success = True
+                except Exception as ex:
+                    log.exception("Fit number %i at %i,%i failed on error %s" % (ii,x,y, str(ex)))
+                    log.exception("Guesses were: {0}".format(str(gg)))
+                    log.exception("Fitkwargs were: {0}".format(str(fitkwargs)))
+                    success = False
+                    if isinstance(ex,KeyboardInterrupt):
+                        raise ex
+
+                # keep this out of the 'try' statement
+                self.has_fit[y,x] = success
+            else:
+                self.has_fit[y,x] = False
+                self.parcube[:,y,x] = blank_value
+                self.errcube[:,y,x] = blank_value
+
+
+            if blank_value != 0:
+                self.parcube[self.parcube == 0] = blank_value
+                self.errcube[self.parcube == 0] = blank_value
+
+            self._counter += 1
+            if verbose:
+                if ii % (min(10**(3-verbose_level),1)) == 0:
+                    snmsg = " s/n=%5.1f" % (max_sn) if max_sn is not None else ""
+                    npix = len(valid_pixels)
+                    pct = 100 * (ii+1.0)/float(npix)
+                    log.info("Finished fit %6i of %6i at (%4i,%4i)%s. Elapsed time is %0.1f seconds.  %%%01.f" %
+                             (ii+1, npix, x, y, snmsg, time.time()-t0, pct))
+
+            if sp.specfit.modelerrs is None:
+                log.exception("Fit number %i at %i,%i failed with no specific error." % (ii,x,y))
+                log.exception("Guesses were: {0}".format(str(gg)))
+                log.exception("Fitkwargs were: {0}".format(str(fitkwargs)))
+                raise TypeError("The fit never completed; something has gone wrong.")
+
+            return ((x,y), sp.specfit.modelpars, sp.specfit.modelerrs)
+
+        if multicore > 1:
+            sequence = [(ii,x,y) for ii,(x,y) in tuple(enumerate(valid_pixels))]
+            result = pyspeckit.cubes.parallel_map(fit_a_pixel, sequence, numcores=multicore)
+            self._result = result # backup - don't want to lose data in the case of a failure
+            # a lot of ugly hacking to deal with the way parallel_map returns
+            # its results needs TWO levels of None-filtering, because any
+            # individual result can be None (I guess?) but apparently (and this
+            # part I don't believe) any individual *fit* result can be None as
+            # well (apparently the x,y pairs can also be None?)
+            merged_result = [core_result for core_result in result if
+                             core_result is not None]
+            # for some reason, every other time I run this code, merged_result
+            # ends up with a different intrinsic shape.  This is an attempt to
+            # force it to maintain a sensible shape.
+            try:
+                ((x,y), m1, m2) = merged_result[0]
+            except ValueError:
+                if verbose > 1:
+                    log.exception("ERROR: merged_result[0] is {0} which has the"
+                                  " wrong shape".format(merged_result[0]))
+                merged_result = itertools.chain.from_iterable(merged_result)
+            for TEMP in merged_result:
+                if TEMP is None:
+                    # this shouldn't be possible, but it appears to happen
+                    # anyway.  parallel_map is great, up to a limit that was
+                    # reached long before this level of complexity
+                    log.debug("Skipped a None entry: {0}".format(str(TEMP)))
+                    continue
+                try:
+                    ((x,y), modelpars, modelerrs) = TEMP
+                except TypeError:
+                    # implies that TEMP does not have the shape ((a,b),c,d)
+                    # as above, shouldn't be possible, but it happens...
+                    log.debug("Skipped a misshapen entry: {0}".format(str(TEMP)))
+                    continue
+                if ((len(modelpars) != len(modelerrs)) or
+                    (len(modelpars) != len(self.parcube))):
+                    raise ValueError("There was a serious problem; modelpar and"
+                                     " error shape don't match that of the "
+                                     "parameter cubes")
+                if np.any(np.isnan(modelpars)) or np.any(np.isnan(modelerrs)):
+                    self.parcube[:,y,x] = np.nan
+                    self.errcube[:,y,x] = np.nan
+                    self.has_fit[y,x] = False
+                else:
+                    self.parcube[:,y,x] = modelpars
+                    self.errcube[:,y,x] = modelerrs
+                    self.has_fit[y,x] = max(modelpars) > 0
+        else:
+            for ii,(x,y) in enumerate(valid_pixels):
+                fit_a_pixel((ii,x,y))
+
+
+        # March 27, 2014: This is EXTREMELY confusing.  This isn't in a loop...
+        # make sure the fitter / fittype are set for the cube
+        # this has to be done within the loop because skipped-over spectra
+        # don't ever get their fittypes set
+        #self.specfit.fitter = sp.specfit.fitter
+        #self.specfit.fittype = sp.specfit.fittype
+        #self.specfit.parinfo = sp.specfit.parinfo
+
+        if verbose:
+            log.info("Finished final fit %i.  "
+                     "Elapsed time was %0.1f seconds" % (len(valid_pixels), time.time()-t0))
+
+
 class SubCubeStack(SubCube, pyspeckit.CubeStack):
     """
     SubCube analogy for CubeStack objects.
@@ -675,3 +975,22 @@ class SubCubeStack(SubCube, pyspeckit.CubeStack):
         super(SubCubeStack, self).__init__(*args)
         log.warn("Doing something horrible with multiple "
                  "inheritance. Needs to be tested more.")
+
+# taken directly from pyspeckit.cubes, 
+# I can't seem to import it for some reason
+def get_neighbors(x, y, shape):
+    """
+    Find the 9 nearest neighbors, excluding self and any out of bounds points
+    """
+    ysh, xsh = shape
+    xpyp = [(ii,jj)
+            for ii,jj in itertools.product((-1,0,1),
+                                           (-1,0,1))
+            if (ii+x < xsh) and (ii+x >= 0)
+            and (jj+y < ysh) and (jj+y >= 0)
+            and not (ii==0 and jj==0)]
+    xpatch, ypatch = zip(*xpyp)
+
+    return np.array(xpatch, dtype='int'), np.array(ypatch, dtype='int')
+
+
