121a2d8d320aa8e4e9b00d93ac826be11e46b298
Vlas Sokolov <vlas145@gmail.com>
2016-11-04 15:11:46 +0100

    fix sinc function + remove trailing whitespaces

--- multicube/astro_toolbox.py
+++ multicube/astro_toolbox.py
@@ -5,7 +5,7 @@ import os
 # TODO: rewrite this to have multiple components generated here.
 #       Having a bundle of test filaments would be very nice.
 
-def make_test_cube(shape=(30,9,9), outfile='test.fits', 
+def make_test_cube(shape=(30,9,9), outfile='test.fits',
                    sigma=None, seed=0, writeSN=False   ):
     """
     Generates a simple gaussian cube with noise of
@@ -18,7 +18,7 @@ def make_test_cube(shape=(30,9,9), outfile='test.fits',
         sigma1d, sigma2d = sigma
 
     gauss1d = Gaussian1DKernel(stddev=sigma1d, x_size=shape[0])
-    gauss2d = Gaussian2DKernel(stddev=sigma2d, x_size=shape[1], 
+    gauss2d = Gaussian2DKernel(stddev=sigma2d, x_size=shape[1],
                                                y_size=shape[2])
     signal_cube = gauss1d.array[:,None,None] * gauss2d.array
     signal_cube=signal_cube/signal_cube.max()
@@ -28,16 +28,16 @@ def make_test_cube(shape=(30,9,9), outfile='test.fits',
                         np.median(signal_cube.std(axis=0))
     test_cube = signal_cube+noise_cube
     true_rms = noise_cube.std()
-    
+
     # making a simple header for the test cube:
     test_hdu = fits.PrimaryHDU(test_cube)
-    # the strange cdelt values are a workaround 
+    # the strange cdelt values are a workaround
     # for what seems to be a bug in wcslib:
     # https://github.com/astropy/astropy/issues/4555
     cdelt1, cdelt2, cdelt3 = -(4e-3+1e-8), 4e-3+1e-8, -0.1
     keylist = {'CTYPE1': 'RA---GLS', 'CTYPE2': 'DEC--GLS', 'CTYPE3': 'VRAD',
                'CDELT1': cdelt1, 'CDELT2': cdelt2, 'CDELT3': cdelt3,
-               'CRVAL1': 0, 'CRVAL2': 0, 'CRVAL3': 5, 
+               'CRVAL1': 0, 'CRVAL2': 0, 'CRVAL3': 5,
                'CRPIX1': 9, 'CRPIX2': 0, 'CRPIX3': 5,
                'CUNIT1': 'deg', 'CUNIT2': 'deg', 'CUNIT3': 'km s-1',
                'BUNIT' : 'K', 'EQUINOX': 2000.0}
@@ -54,7 +54,7 @@ def make_test_cube(shape=(30,9,9), outfile='test.fits',
     if writeSN:
         signal_hdu = fits.PrimaryHDU(data=signal_cube, header=test_header)
         noise_hdu  = fits.PrimaryHDU(data=noise_cube , header=test_header)
-        signame, noiname = [outfile.split('.fits')[0]+'-'+i+'.fits' 
+        signame, noiname = [outfile.split('.fits')[0]+'-'+i+'.fits'
                                             for i in ['signal','noise']]
         signal_hdu.writeto(signame, clobber=True, checksum=True)
         noise_hdu.writeto( noiname, clobber=True, checksum=True)
@@ -95,12 +95,12 @@ def tinker_ring_parspace(parseed, xy_shape, parindices=[], paramps=[]):
     yarr, xarr = np.indices(xy_shape)
     cent = (np.array(xy_shape)-1)/2.
     arm = (min(xy_shape)-1)/2.
-    dist_norm = np.sqrt(((np.array([xarr,yarr]) - 
+    dist_norm = np.sqrt(((np.array([xarr,yarr]) -
                           cent[:,None,None])**2).sum(axis=0)) / arm
 
     # a pretty distort function
     c = 1.5*np.pi # normalization constant for radial distance
-    f = lambda x: (np.sin(x*c)**2 / (x*c)**2 + np.cos(x*c)**2)
+    f = lambda x: (np.sinc(x*c)**2 + np.cos(x*c)**2)
 
     for par_idx, par_amp in zip(parindices, paramps):
         xy_pars[par_idx] += (f(dist_norm)-1) * par_amp
