b668de533246ded2ae10d2dcd537cbf0552e4f22
Vlas Sokolov <vlas145@gmail.com>
2018-03-14 19:08:30 +0100

    fix examples: pep8, py3

--- examples/example-gauss-2d.py
+++ examples/example-gauss-2d.py
@@ -1,5 +1,5 @@
 """
-A working example script: 
+A working example script:
 * generates a spectral FITS cube
 * makes a guess grid based on given parameter ranges
 * makes best guess calculatons and goodness-of-fit estimates
@@ -11,7 +11,7 @@ from multicube.astro_toolbox import make_test_cube
 from multiprocessing import cpu_count
 
 # generating a dummy FITS file
-make_test_cube((300,10,10), outfile='foo.fits', sigma=(10,5))
+make_test_cube((300, 10, 10), outfile='foo.fits', sigma=(10, 5))
 sc = SubCube('foo.fits')
 
 # TODO: move this to astro_toolbox.py
@@ -20,39 +20,43 @@ sc = SubCube('foo.fits')
 # this will introduce a radial velocity gradient:
 def tinker_ppv(arr):
     scale_roll = 15
-    rel_shift  = 30
-    rel_str    = 5
+    rel_shift = 30
+    rel_str = 5
     shifted_component = np.roll(arr, rel_shift) / rel_str
-    for y,x in np.ndindex(arr.shape[1:]):
-        roll  = np.sqrt((x-5)**2 + (y-5)**2) * scale_roll
-        arr[:,y,x] = np.roll(arr[:,y,x], int(roll))
+    for y, x in np.ndindex(arr.shape[1:]):
+        roll = np.sqrt((x - 5)**2 + (y - 5)**2) * scale_roll
+        arr[:, y, x] = np.roll(arr[:, y, x], int(roll))
     return arr + shifted_component
 sc.cube = tinker_ppv(sc.cube)
 
 sc.update_model('gaussian')
+npeaks = 2
+sc.specfit.npeaks = npeaks
+sc.specfit.fitter.npeaks = npeaks
+sc.specfit.fitter.make_parinfo(npeaks=npeaks)
 
 main_comp = [[0.1, sc.xarr.min().value, 0.1], # minpars
              [2.0, sc.xarr.max().value, 2.0], # maxpars
-             [10 ,                  10,  10], # finesse
-             [False]*3, # fixed
-             [True]*3,  # limitedmin
-             [True]*3]  # limitedmax
+             [10, 10, 10], # finesse
+             [False] * 3, # fixed
+             [True] * 3, # limitedmin
+             [True] * 3] # limitedmax
 
 sidekick = [[0.1, -10, 0.1], # minpars
             [0.5, -10, 0.5], # maxpars
-            [  3,   1,   3], # finesse
-            [False]*3, # fixed
-            [True]*3,  # limitedmin
-            [True]*3]  # limitedmax
+            [3, 1, 3], # finesse
+            [False] * 3, # fixed
+            [True] * 3, # limitedmin
+            [True] * 3] # limitedmax
 
 total_zero = [[0.0, 5, 1.0], # minpars
               [0.0, 5, 1.0], # maxpars
-              [  1, 1,   1], # finesse
-              [True]*3, # fixed
-              [True]*3,  # limitedmin
-              [True]*3]  # limitedmax
+              [1, 1, 1], # finesse
+              [True] * 3, # fixed
+              [True] * 3, # limitedmin
+              [True] * 3] # limitedmax
 
-unpack = lambda a,b: [i+j for i,j in zip(a,b)]
+unpack = lambda a, b: [i + j for i, j in zip(a, b)]
 
 # Estimating SNR
 sc.get_snr_map()
@@ -62,14 +66,15 @@ sc.make_guess_grid(*unpack(main_comp, total_zero))
 sc.expand_guess_grid(*unpack(main_comp, sidekick))
 
 # Generating spectral models for all guesses
-sc.generate_model()
+sc.generate_model(multicore=cpu_count() - 1)
+
 # Calculating the best guess on the grid
 sc.best_guess()
 
-sc.fiteach(fittype   = sc.fittype,
-           guesses   = sc.best_guesses,
-           multicore = cpu_count()-1,
-           errmap    = sc._rms_map,
+sc.fiteach(fittype = sc.fittype,
+           guesses = sc.best_guesses,
+           multicore = cpu_count() - 1,
+           errmap = sc._rms_map,
            **sc.best_fitargs)
 
 # computing chi^2 statistics to judge the goodness of fit:
--- examples/example-gauss.py
+++ examples/example-gauss.py
@@ -1,5 +1,5 @@
 """
-A working example script: 
+A working example script:
 * generates a spectral FITS cube
 * makes a guess grid based on given parameter ranges
 * makes best guess calculatons and goodness-of-fit estimates
@@ -7,14 +7,14 @@ A working example script:
 import numpy as np
 import matplotlib.pylab as plt
 import pyspeckit
-from subcube import SubCube
-from astro_toolbox import make_test_cube
-from astro_toolbox import get_ncores
+from multicube.subcube import SubCube
+from multicube.astro_toolbox import make_test_cube
+from multicube.astro_toolbox import get_ncores
 
 # TODO: rewrite into a jupyter notebook example!
 
 # generating a dummy FITS file
-make_test_cube((300,10,10), outfile='foo.fits', sigma=(10,5))
+make_test_cube((300, 10, 10), outfile='foo.fits', sigma=(10, 5))
 sc = SubCube('foo.fits')
 
 # TODO: move this to astro_toolbox.py
@@ -23,12 +23,12 @@ sc = SubCube('foo.fits')
 # this will introduce a radial velocity gradient:
 def tinker_ppv(arr):
     scale_roll = 15
-    rel_shift  = 30
-    rel_str    = 5
+    rel_shift = 30
+    rel_str = 5
     shifted_component = np.roll(arr, rel_shift) / rel_str
-    for y,x in np.ndindex(arr.shape[1:]):
-        roll  = np.sqrt((x-5)**2 + (y-5)**2) * scale_roll
-        arr[:,y,x] = np.roll(arr[:,y,x], int(roll))
+    for y, x in np.ndindex(arr.shape[1:]):
+        roll = np.sqrt((x - 5)**2 + (y - 5)**2) * scale_roll
+        arr[:, y, x] = np.roll(arr[:, y, x], int(roll))
     return arr + shifted_component
 sc.cube = tinker_ppv(sc.cube)
 
@@ -38,25 +38,25 @@ minpars = [0.1, sc.xarr.min().value, 0.1]
 maxpars = [2.0, sc.xarr.max().value, 2.0]
 finesse = 10
 
-print "Estimating SNR . . ."
+#print "Estimating SNR . . ."
 sc.get_snr_map()
 
-print "Making a guess grid based on parameter permutations . . ."
+print("Making a guess grid based on parameter permutations . . .")
 sc.make_guess_grid(minpars, maxpars, finesse)
-print "Generating spectral models for all %i guesses . . ." \
-                    % sc.guess_grid.shape[0]
+print("Generating spectral models for all {}"
+      " guesses . . .".format(sc.guess_grid.shape[0]))
 sc.generate_model()
-print "Calculating the best guess on the grid . . ."
+print("Calculating the best guess on the grid . . .")
 sc.best_guess()
 
 sc1, sc2 = sc, sc.copy()
 
 # fitting the whole cube with a single guess, selected
-# to be the best for the pixel with the highest S/N ratio
-sc1.fiteach(fittype   = sc1.fittype,
-            guesses   = sc1.best_snr_guess,
+# to be the best among all the pixels
+sc1.fiteach(fittype = sc1.fittype,
+            guesses = sc1.best_overall,
             multicore = get_ncores(),
-            errmap    = sc1._rms_map,
+            errmap = sc1._rms_map,
             **sc1.fiteach_args)
 
 # computing chi^2 statistics to judge the goodness of fit:
@@ -64,7 +64,7 @@ sc1.get_chi_squared(sigma=sc1.header['RMSLVL'])
 sc1.chi_squared_stats(plot_chisq=True)
 
 # let's plot the velocity field:
-#sc1.show_fit_param(1, cmap='coolwarm')
+sc1.show_fit_param(1, cmap='coolwarm')
 clb = sc1.mapplot.FITSFigure.colorbar
 clb.set_axis_label_text(sc1.xarr.unit.to_string('latex_inline'))
 
@@ -73,10 +73,10 @@ sc1.mark_bad_fits(cut = 1e-40) # not so good at the edges, right?
 # (the velocity centroid varies too much across the map)
 
 # now let's pass fiteach the best guesses for every x,y pixel!
-sc2.fiteach(fittype   = sc2.fittype,
-            guesses   = sc2.best_guesses,
+sc2.fiteach(fittype = sc2.fittype,
+            guesses = sc2.best_guesses,
             multicore = get_ncores(),
-            errmap    = sc2._rms_map,
+            errmap = sc2._rms_map,
             **sc2.fiteach_args)
 
 # computing chi^2 statistics to judge the goodness of fit:
